abc096c6a03ea90eec6180b6fc5284a85849af92
roeck <rons9n@gmail.com>
2021-12-22 17:09:06 -0500

    Done

---
+++ src/components/Bands.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//recieve props from container
+
+const Bands = props => {
+    const bands = props.bands.map((band, index) => {
+      return <li key={index}>{band.name}</li>;
+    });
+  
+    return (
+      <div>
+        {bands}
+      </div>
+    ); 
+};
+
+export default Bands;
